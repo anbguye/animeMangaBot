bc39cd657d1d075a30aa1a84ebde1ad6665db89a
antho <antho@Anthony.mshome.net>
2024-05-14 11:44:52 -0500

    Added bot responses for when user inputs !remove, !read, and !edit.

--- animeMangaBot/src/animeMangaBot.java
+++ animeMangaBot/src/animeMangaBot.java
@@ -30,6 +30,42 @@ public class animeMangaBot {
 			if (userInput.startsWith("!add")) {
 				String[] input = userInput.split("\\s+", 3);
 				myList.add(new animeManga(Integer.parseInt(input[1]), input[2]));
+				return event.getMessage().getChannel()
+						.flatMap(channel -> channel.createMessage("Added: " + input[2] + " Chapter " + input[1]));
+			} else if (userInput.startsWith("!remove")) {
+				String[] input = userInput.split("\\s+", 2);
+				boolean removed = myList.removeIf(val -> val.getAnimeManga().equalsIgnoreCase(input[1]));
+				if (removed)
+					return event.getMessage().getChannel()
+							.flatMap(channel -> channel.createMessage("Removed: " + input[1]));
+
+				return event.getMessage().getChannel()
+						.flatMap(channel -> channel.createMessage("Unable to find title."));
+			} else if (userInput.startsWith("!read")) {
+				String[] input = userInput.split("\\s+", 2);
+
+				for (animeManga animanga : myList)
+					if (animanga.getAnimeManga().equals(input[1])) {
+						animanga.addChapter();
+						return event.getMessage().getChannel().flatMap(channel -> channel
+								.createMessage("Currently finished with: " + animanga.getChapter() + " chapters."));
+					}
+
+				return event.getMessage().getChannel()
+						.flatMap(channel -> channel.createMessage("Unable to find title."));
+
+			} else if (userInput.startsWith("!edit")) {
+				String[] input = userInput.split("\\s+", 3);
+
+				for (animeManga animanga : myList)
+					if (animanga.getAnimeManga().equalsIgnoreCase(input[2])) {
+						animanga.setChapter(Integer.parseInt(input[1]));
+						return event.getMessage().getChannel().flatMap(channel -> channel
+								.createMessage("Now currently read up to chapter: " + animanga.getChapter()));
+					}
+
+				return event.getMessage().getChannel()
+						.flatMap(channel -> channel.createMessage("Unable to find title."));
 			}
 
 			return Mono.empty();
